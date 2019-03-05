297d6d6fb7d851f8c2c0f2dacbca32429ec9d084
ST3VI3 RICHI3 <30847371+ST3VI3-RICHI3@users.noreply.github.com>
2019-03-05 20:53:06 +0000

    Added update feature, optomised loading token, added help command. Config command is not implemented but will update files for the bot remotley should the need arise.

--- bot.py
+++ bot.py
@@ -8,11 +8,12 @@ import time
 import os
 import threading
 
-print("Bot loading token from file TOKEN.txt")
+print("Bot loading token from file TOKEN")
 
-Reader = open("TOKEN.txt", "r")
-TOKEN = str(Reader.readline())
+TOKEN = open(str("TOKEN"), "r")
+TOKEN = str(TOKEN.readline())
 
+print("Done!")
 print("Bot loading...")
 
 bot = commands.Bot(command_prefix='p!')#sets the prefix for the bot.
@@ -21,6 +22,29 @@ bot.remove_command('help')
 @bot.event
 async def on_ready():
 	await bot.change_presence(game=discord.Game(name="p!help"), status=discord.Status("online"), afk=False)
-	print("Bot is running & ready!")
-	
+	print("Done! Bot is running & ready!")
+
+@bot.command(pass_context = True)
+async def help(ctx):
+	await bot.say("This is the help command, it is not implemented.")
+
+@bot.command(pass_context = True)
+async def Config(ctx, File : str, Property : str, Value : str):
+	print("Not implemented")
+
+@bot.command(pass_context = True)
+async def restart(ctx):
+	await bot.change_presence(game=discord.Game(name="Restarting..."), status=discord.Status("dnd"), afk=False)
+	print("Restarting...")
+	os.system("start bot.py")
+	exit(0)
+
+@bot.command(pass_context = True)
+async def update(ctx):
+	await bot.change_presence(game=discord.Game(name="Updating..."), status=discord.Status("dnd"), afk=False)
+	print("Downloading update from git...")
+	os.system("git pull")
+	os.system("start bot.py")
+	exit(0)
+
 bot.run(TOKEN)
